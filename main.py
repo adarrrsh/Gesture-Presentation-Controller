9059cba3aa1a77f8f6740d6487703bc6b1d0c2d2
AdarshTechnical <132006822+adarrrsh@users.noreply.github.com>
2023-12-18 22:51:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import os
+import numpy as np
+
+
+width, height = 1280,720
+folderPath = "Presentation"
+cap = cv2.VideoCapture(0)
+cap.set(3,width)
+cap.set(4,height)
+
+pathImages = sorted(os.listdir(folderPath),key=len)
+# print(pathImages)
+imgNumber = 0
+
+hs,ws = int(120*1.2),int(213*1.2)
+gestureThreshold = 300
+buttonPressed = False
+buttonCounter = 0
+buttonDelay = 30
+detector = HandDetector(detectionCon=0.8,maxHands=1)
+annotations = [[]]
+annotationNumber = -1
+annotationStart = False
+
+while True:
+    sucess,img = cap.read()
+    img = cv2.flip(img,1)
+    pathFullImage = os.path.join(folderPath,pathImages[imgNumber])
+    imgCurrent = cv2.imread(pathFullImage)
+
+    hands, img = detector.findHands(img)
+    cv2.line(img,(0,gestureThreshold),(width,gestureThreshold),(0,255,0),10)
+    if hands and buttonPressed is False:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        cx,cy = hand['center']
+        lmList = hand['lmList']
+        indexFinger = lmList[8][0], lmList[8][1]
+
+        xVal = int(np.interp(lmList[8][0],[width//2,w],[0,width]))
+        yVal = int(np.interp(lmList[8][1],[150,height-150],[0,height]))
+        indexFinger = xVal,yVal
+
+        # print(fingers)
+        # if cy <= gestureThreshold:
+        if fingers == [1,1,0,0,0]:
+            print("Left")
+            if imgNumber > 0:
+                imgNumber -= 1
+                buttonPressed = True
+
+        if fingers == [1,1,1,1,1]:
+            print("Right")
+            if imgNumber < len(pathImages)-1:
+                imgNumber +=1
+                buttonPressed = True
+        if fingers == [0,1,0,0,0]:
+            cv2.circle(imgCurrent,indexFinger,12,(0,0,255),cv2.FILLED)
+        if fingers == [0,1,1,0,0]:
+            if annotationStart is False:
+                annotationStart = True
+                annotationNumber += 1
+                annotations.append([])
+            cv2.circle(imgCurrent,indexFinger,12,(0,0,255),cv2.FILLED)
+            annotations[annotationNumber].append(indexFinger)
+        else:
+            annotationStart = False
+
+    if buttonPressed:
+        buttonCounter += 1
+        if buttonCounter > buttonDelay:
+            buttonCounter = 0
+            buttonPressed = False
+    for i in range(len(annotations)):
+        for j in range(len(annotations[i])):
+            if j != 0:
+                cv2.line(imgCurrent,annotations[i][j-1],annotations[i][j],(0,0,200),12)
+
+
+    imgSmall = cv2.resize(img,(ws,hs)) 
+    h,w,_ = imgCurrent.shape
+    imgCurrent[0:hs,w-ws:w] = imgSmall
+
+    # cv2.imshow("image",img)
+    cv2.imshow("Slides",imgCurrent)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    
