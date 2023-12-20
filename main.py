7d26253945dbdc2beaee58fd30e2315e02bfc7c6
AdarshTechnical <132006822+adarrrsh@users.noreply.github.com>
2023-12-20 11:16:45 +0530

    Project done - 20/12/2023 (20th December 2023)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3,width)
 cap.set(4,height)
 
-pathImages = sorted(os.listdir(folderPath),key=len)
+pathImages = sorted(os.listdir(folderPath), key=len)
 # print(pathImages)
 imgNumber = 0
 
@@ -19,7 +19,7 @@ gestureThreshold = 300
 buttonPressed = False
 buttonCounter = 0
 buttonDelay = 30
-detector = HandDetector(detectionCon=0.8,maxHands=1)
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 annotations = [[]]
 annotationNumber = -1
 annotationStart = False
@@ -27,11 +27,11 @@ annotationStart = False
 while True:
     sucess,img = cap.read()
     img = cv2.flip(img,1)
-    pathFullImage = os.path.join(folderPath,pathImages[imgNumber])
+    pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
     imgCurrent = cv2.imread(pathFullImage)
 
     hands, img = detector.findHands(img)
-    cv2.line(img,(0,gestureThreshold),(width,gestureThreshold),(0,255,0),10)
+    cv2.line(img,(0,gestureThreshold),(width, gestureThreshold), (0,255,0),10)
     if hands and buttonPressed is False:
         hand = hands[0]
         fingers = detector.fingersUp(hand)
@@ -39,25 +39,34 @@ while True:
         lmList = hand['lmList']
         indexFinger = lmList[8][0], lmList[8][1]
 
-        xVal = int(np.interp(lmList[8][0],[width//2,w],[0,width]))
-        yVal = int(np.interp(lmList[8][1],[150,height-150],[0,height]))
+        xVal = int(np.interp(lmList[8][0], [width//2,w],[0,width]))
+        yVal = int(np.interp(lmList[8][1], [150, height-150],[0, height]))
         indexFinger = xVal,yVal
 
         # print(fingers)
         # if cy <= gestureThreshold:
+        # annotationStart = False
         if fingers == [1,1,0,0,0]:
             print("Left")
             if imgNumber > 0:
                 imgNumber -= 1
                 buttonPressed = True
+                annotations = [[]]
+                annotationNumber = -1
+                annotationStart = False
 
         if fingers == [1,1,1,1,1]:
             print("Right")
             if imgNumber < len(pathImages)-1:
                 imgNumber +=1
                 buttonPressed = True
+                annotations = [[]]
+                annotationNumber = -1
+                annotationStart = False
+
         if fingers == [0,1,0,0,0]:
             cv2.circle(imgCurrent,indexFinger,12,(0,0,255),cv2.FILLED)
+            annotationStart = False
         if fingers == [0,1,1,0,0]:
             if annotationStart is False:
                 annotationStart = True
@@ -67,6 +76,13 @@ while True:
             annotations[annotationNumber].append(indexFinger)
         else:
             annotationStart = False
+        if fingers == [0,1,1,1,0]:
+            if annotations:
+                annotations.pop(-1)
+                annotationNumber -= 1
+                buttonPressed = True
+    else:
+        annotationStart = False
 
     if buttonPressed:
         buttonCounter += 1
